1aebe49f41cc522598ad7418d3f46f4d82614a83
ilovecodingallday <ilovecodingallday@gmail.com>
2022-09-06 11:32:32 -0400

    first commit

---
+++ src/main/java/com/mcm/javaserver/Controller.java
@@ -0,0 +1,14 @@
+package com.mcm.javaserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping()
+    public String home() {
+        return "Home";
+    }
+
+
+}
